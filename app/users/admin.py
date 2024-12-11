b375005f39bbbd8a5acccdd3b071c58630a152b4
mezamateoj <mezamateoj@gmail.com>
2024-12-11 17:10:45 -0300

    Add admin functionality and tests for user management
    - Implemented custom UserAdmin for managing users in the Django admin interface.
    - Added tests for user listing, editing, and creation in the admin site.
    - Created new files for admin configuration and corresponding tests.

--- app/users/admin.py
+++ app/users/admin.py
@@ -1,2 +1,32 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from users.models import User
 
-# Register your models here.
+# https://docs.djangoproject.com/en/5.1/topics/auth/customizing/#custom-users-admin-full-example
+class UserAdmin(BaseUserAdmin):
+    """Define the admin pages for users"""
+
+    ordering = ['id']
+    list_display = ['email', 'name']
+
+    fieldsets = [
+        (None, {"fields": ["email", "password"]}),
+        ("Permissions", {"fields": ["is_active", "is_staff", "is_superuser"]}),
+        ("Dates", {"fields": ["last_login", "created_on"]}),
+
+    ]
+
+    add_fieldsets = [
+        (
+            None,
+            {
+                "classes": ["wide"],
+                "fields": ["email", "name", "password1", "password2", 'is_active', 'is_staff', 'is_superuser'],
+            },
+        ),
+    ]
+
+    search_fields = ["email"]
+    readonly_fields = ['last_login', 'created_on']
+
+admin.site.register(User, UserAdmin)
